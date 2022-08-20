f1b8dc5cfa345e87fb75a84b45061e069966ab7b
MD.Sakib Naim <98878762+SakibNaim@users.noreply.github.com>
2022-08-20 20:16:40 +0600

    Add files via upload

---
+++ lab task 9/nav.php
@@ -0,0 +1,9 @@
+ <ul>
+        <li><a href="adduser.php"> ad user</a></li>
+        <li><a href="showAllUser.php"> Show all user</a></li>
+        <li><a href="searchUser.php"> searchUser</a></li>
+        
+
+        
+
+    </ul>
